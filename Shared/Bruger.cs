c95ccab8679d98604143d997ab5a5e3cb1331d83
sarahfahlen <sarah.fahlen@live.dk>
2025-05-07 11:47:34 +0200

    Oprettelse af bruger + lokation klasse

--- Shared/Bruger.cs
+++ Shared/Bruger.cs
@@ -1,6 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace Shared;
 
 public class Bruger
 {
-    
+    [BsonId] // Brug denne som ID i stedet for _id
+    [BsonRepresentation(BsonType.Int32)] 
+    public int BrugerId { get; set; }
+    public string Navn { get; set; }
+    public string Email { get; set; }
+    public string Adgangskode { get; set; }
+    public int Telefon { get; set; }
+    public string Rolle { get; set; }
+    public string Billede { get; set; }
+    public DateOnly StartDato { get; set; }
+    public Lokation Koekken { get; set; }
 }
--- Shared/Lokation.cs
+++ Shared/Lokation.cs
@@ -1,6 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace Shared;
 
 public class Lokation
 {
-    
+   [BsonId] // Brug denne som ID i stedet for _id
+   [BsonRepresentation(BsonType.Int32)] 
+   public int LokationId { get; set; } 
+   public string Navn { get; set; }
+   public string Adresse { get; set; }
+   public int Telefon { get; set; }
+   public string Type { get; set; }
 }
