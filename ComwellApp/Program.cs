cdd9839bca9e30e0a19f52af10cf5a09282b0ef8
MohammadHHaj <muddihhaj@hotmail.com>
2025-05-07 12:10:41 +0200

    Login

--- ComwellApp/Program.cs
+++ ComwellApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ComwellApp;
@@ -6,7 +7,9 @@ using ComwellApp.Services.Login;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<ILoginService, LoginServiceClientSite>(); 
+
 await builder.Build().RunAsync();
