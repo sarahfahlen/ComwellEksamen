6a87bf61f0d4ef5da566531c0dede257549f0642
FrederikSA <eaa24fsan@students.eaaa.dk>
2025-05-13 14:08:44 +0200

    filtrer knap til gennemført

--- ComwellApp/Services/Elevplan/IElevplanService.cs
+++ ComwellApp/Services/Elevplan/IElevplanService.cs
@@ -19,6 +19,7 @@ public interface IElevplanService
     //Bruges til at oprette default skabelon til nye elever
     public Task<Shared.Elevplan> LavDefaultSkabelon(Bruger ansvarlig);
     //Bruges til at hente de filtrede mål på elevplanen
-    List<Maal> HentFiltreredeMaal(Shared.Elevplan plan, int periodeIndex, string? valgtMaalNavn, string? valgtDelmaalType, string? søgeord);
+    List<Maal> HentFiltreredeMaal(Shared.Elevplan plan, int periodeIndex, string? valgtMaalNavn, string? valgtDelmaalType, string? søgeord, bool? filterStatus);
+
 
 }
