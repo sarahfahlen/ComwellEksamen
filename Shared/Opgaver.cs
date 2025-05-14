bed7d8950132dc503b5e746a9a90162d7e40b7b7
MohammadHHaj <muddihhaj@hotmail.com>
2025-05-14 09:12:25 +0200

    id

--- Shared/Opgaver.cs
+++ Shared/Opgaver.cs
@@ -6,7 +6,7 @@ public class Opgaver
 {
     [BsonId] // Brug denne som ID i stedet for _id
     [BsonRepresentation(BsonType.Int32)] 
-    public int OpgaveID { get; set; } 
+    public int OpgaveId { get; set; } 
     public string OpgaveNavn { get; set; }
     public bool OpgaveGennemfoert { get; set; }
     public string OpgaveDetaljer { get; set; }
