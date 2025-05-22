8f411f3ed0931c2b4750d734e2fb08baa3c8bf7c
FrederikSA <eaa24fsan@students.eaaa.dk>
2025-05-22 22:02:13 +0200

    update til learning

--- Shared/Element.cs
+++ Shared/Element.cs
@@ -8,6 +8,8 @@ public class Element
     public int ElementId { get; set; }
     public string ElementNavn { get; set; }
     public string ElementBeskrivelse { get; set; }
+    public string? Beskrivelse { get; set; }
     public string ElementType { get; set; } 
+    
     public List<Spoergsmaal> ListSpoergsmaal { get; set; } = new();
 }
