2a98f75d0bf19c60cfb2e20d78d777edaf30a8e2
FrederikSA <eaa24fsan@students.eaaa.dk>
2025-05-23 13:32:10 +0200

    http

--- ComwellApp/Program.cs
+++ ComwellApp/Program.cs
@@ -16,7 +16,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // HttpClient til API-kald (til backend)
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("https://localhost:5237")
+    BaseAddress = new Uri("http://localhost:5237")
     // BaseAddress = new Uri("https://comwellworkflow.azurewebsites.net")
 });
 
